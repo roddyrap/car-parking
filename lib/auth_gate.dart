34023f3de91c4c4214ab7d232170139fb33f2d80
Roddy Rappaport <roddy.rappaport@gmail.com>
2026-04-18 16:59:39 +0300

    Deleting loading indicator from the Flutter app
    Hopefully this will provide a more seamless transition.

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,23 +1,49 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
 
 import 'package:car_parking_tracker/cars_view/cars_page.dart';
 
-class AuthGate extends StatelessWidget {
+class AuthGate extends StatefulWidget {
   const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  bool _loadingSignRemoved = false;
+
+  void _removeLoadingSign() {
+    if (_loadingSignRemoved) return;
+    _loadingSignRemoved = true;
+
+    // Wait until the current frame finishes rendering.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final loaderElement = web.document.querySelector('#loading-screen');
+      loaderElement?.remove();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          // From firebase_ui_auth
-          return SignInScreen();
+        // Return nothing becuase the HTML loading indicator exists.
+        print("Autgate connection state: ${snapshot.connectionState}");
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedBox.shrink(); 
         }
 
-        return CarsPage(); 
+        _removeLoadingSign();
+
+        if (!snapshot.hasData) {
+          return const SignInScreen();
+        }
+
+        return const CarsPage(); 
       },
     );
   }
